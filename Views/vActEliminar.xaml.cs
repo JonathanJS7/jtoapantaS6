308d50ce957ca51126ad92fbe471f684301ad790
Jonathan <jonathan_js7@hotmail.com>
2024-07-09 21:44:21 -0500

    claseS7

--- Views/vActEliminar.xaml.cs
+++ Views/vActEliminar.xaml.cs
@@ -1,4 +1,5 @@
 using jtoapantaS6.Models;
+using System.Net;
 
 namespace jtoapantaS6.Views;
 
@@ -15,15 +16,22 @@ public partial class vActEliminar : ContentPage
 
     private void btnActualizar_Clicked(object sender, EventArgs e)
     {
-		try
-		{
+        try
+        {
+            WebClient cliente = new WebClient();
+            var parametros = new System.Collections.Specialized.NameValueCollection();
+            parametros.Add("nombre", txtNombre.Text);
+            parametros.Add("apellido", txtApellido.Text);
+            parametros.Add("edad", txtEdad.Text);
 
-		}
-		catch (Exception)
-		{
+            cliente.UploadValues("http://192.168.56.1/semana6/estudiantews.php", "PUT", parametros);
+            Navigation.PushAsync(new vEstudiante());
+        }
+        catch (Exception ex)
+        {
 
-			throw;
-		}
+            DisplayAlert("Alerta", ex.Message, "Ok");
+        }
     }
 
     private void btnEliminar_Clicked(object sender, EventArgs e)
