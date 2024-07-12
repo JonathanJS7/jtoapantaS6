3652e0006204aac9606be71ab32c1762407001a7
Jonathan <jonathan_js7@hotmail.com>
2024-07-12 00:15:09 -0500

    TareaSemana7

--- Views/vActEliminar.xaml.cs
+++ Views/vActEliminar.xaml.cs
@@ -1,49 +1,62 @@
 using jtoapantaS6.Models;
 using System.Net;
+using System.Text;
 
 namespace jtoapantaS6.Views;
 
 public partial class vActEliminar : ContentPage
 {
-	public vActEliminar(Estudiante datos)
-	{
-		InitializeComponent();
-		txtCodigo.Text= datos.codigo.ToString();
-		txtNombre.Text = datos.nombre;
-		txtApellido.Text = datos.apellido;
-		txtEdad.Text= datos.edad.ToString();
-	}
 
-    private void btnActualizar_Clicked(object sender, EventArgs e)
+    private static readonly HttpClient client = new HttpClient();
+
+    public vActEliminar(Estudiante datos)
+    {
+        InitializeComponent();
+        txtCodigo.Text = datos.codigo.ToString();
+        txtNombre.Text = datos.nombre;
+        txtApellido.Text = datos.apellido;
+        txtEdad.Text = datos.edad.ToString();
+    }
+
+    private async void btnActualizar_Clicked(object sender, EventArgs e)
     {
         try
         {
             WebClient cliente = new WebClient();
             var parametros = new System.Collections.Specialized.NameValueCollection();
+            parametros.Add("codigo", txtCodigo.Text);
             parametros.Add("nombre", txtNombre.Text);
             parametros.Add("apellido", txtApellido.Text);
             parametros.Add("edad", txtEdad.Text);
 
-            cliente.UploadValues("http://192.168.56.1/semana6/estudiantews.php", "PUT", parametros);
-            Navigation.PushAsync(new vEstudiante());
+            string url = "http://192.168.56.1/semana6/estudiantews.php";
+            string query = $"?codigo={txtCodigo.Text}&nombre={txtNombre.Text}&apellido={txtApellido.Text}&edad={txtEdad.Text}";
+
+            cliente.UploadValues(url + query, "PUT", parametros);
+            await DisplayAlert("Éxito", "Estudiante actualizado correctamente", "Ok");
+            await Navigation.PushAsync(new vEstudiante());
         }
         catch (Exception ex)
         {
-
-            DisplayAlert("Alerta", ex.Message, "Ok");
+            await DisplayAlert("Alerta", ex.Message, "Ok");
         }
     }
 
-    private void btnEliminar_Clicked(object sender, EventArgs e)
+    private async void btnEliminar_Clicked(object sender, EventArgs e)
     {
-		try
-		{
+        try
+        {
+            WebClient cliente = new WebClient();
+            string url = "http://192.168.56.1/semana6/estudiantews.php";
+            string query = $"?codigo={txtCodigo.Text}";
 
-		}
-		catch (Exception)
-		{
-
-			throw;
-		}
+            cliente.UploadValues(url + query, "DELETE", new System.Collections.Specialized.NameValueCollection());
+            await DisplayAlert("Éxito", "Estudiante eliminado correctamente", "Ok");
+            await Navigation.PushAsync(new vEstudiante());
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Alerta", ex.Message, "Ok");
+        }
     }
 }
